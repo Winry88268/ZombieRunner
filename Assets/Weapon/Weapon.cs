1d4800e226f28b2b91a27cc20a74061203118c2b
Naea Fairbrother <NaeaFairbrother@gmail.com>
2021-11-29 03:30:10 +0000

    Hit VFX

--- Assets/Weapon/Weapon.cs
+++ Assets/Weapon/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float rayRange = 100f;
     [SerializeField] float bulletDamage = 25f;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -37,6 +38,7 @@ public class Weapon : MonoBehaviour
                         out hit,
                         this.rayRange))
         {
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null)
             {
@@ -49,5 +51,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(this.hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.1f);
     }
 }
