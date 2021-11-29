450412b77d167e99894d0a3606c0bfb80a1ea4e5
Naea Fairbrother <NaeaFairbrother@gmail.com>
2021-11-29 02:50:19 +0000

    Muzzle Flash VFX

--- Assets/Weapon/Weapon.cs
+++ Assets/Weapon/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float rayRange = 100f;
     [SerializeField] float bulletDamage = 25f;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -19,17 +20,28 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        this.muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(this.FPCamera.transform.position, 
-                        this.FPCamera.transform.forward, 
-                        out hit, 
+        if (Physics.Raycast(this.FPCamera.transform.position,
+                        this.FPCamera.transform.forward,
+                        out hit,
                         this.rayRange))
         {
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if(target == null)
+            if (target == null)
             {
                 return;
-            }   
+            }
 
             target.TakeDamage(this.bulletDamage);
         }
